1d5c4da57f267d8a91a2f559e7830c3d9ebd8e56
charlie <commiter@papayecube.com>
2020-05-28 10:50:09 +0200

    add topics creation/deletion

---
+++ lib/procon/topics.ex
@@ -0,0 +1,52 @@
+defmodule Procon.Topics do
+  @spec create_topic(binary, pos_integer()) :: {:error, any} | :ok
+  def create_topic(topic, num_partitions) when is_binary(topic) do
+    :brod.create_topics(
+      Application.get_env(:procon, :brokers),
+      [
+        %{
+          config_entries: [],
+          num_partitions: num_partitions,
+          replica_assignment: [],
+          replication_factor: 1,
+          topic: topic
+        }
+      ],
+      %{timeout: 1000}
+    )
+  end
+
+  @spec create_topic(
+          atom
+          | %{
+              optional(:brokers) => false | nil | [{atom | binary | [any], pos_integer}],
+              optional(:timeout) => %{
+                optional(:timeout) => pos_integer,
+                optional(:validate_only) => boolean
+              },
+              required(:topics) => [
+                [{any, any}] | %{optional(atom) => atom | binary | [any] | integer | map}
+              ]
+            }
+        ) :: {:error, any} | :ok
+  def create_topic(config) when is_map(config) do
+    :brod.create_topics(
+      config.brokers || Application.get_env(:procon, :brokers),
+      config.topics,
+      config.timeout || %{timeout: 1000}
+    )
+  end
+
+  @spec delete_topic(%{
+          optional(:brokers) => nil | [{atom | binary, pos_integer}],
+          required(:topic) => [atom | binary],
+          optional(:timeout) => pos_integer
+        }) :: {:error, any} | :ok
+  def delete_topic(config) do
+    :brod.delete_topics(
+      Map.get(config, :brokers, Application.get_env(:procon, :brokers)),
+      Map.get(config, :topics),
+      Map.get(config, :tiemout, 1000)
+    )
+  end
+end
