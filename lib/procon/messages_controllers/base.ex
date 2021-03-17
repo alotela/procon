bb75a92cbc88409e847a4c7c898ce6c002302685
Rocky Signavong <rsignavong@gmail.com>
2021-03-17 19:23:45 +0100

    move accessors method outside macro Base

--- lib/procon/messages_controllers/base.ex
+++ lib/procon/messages_controllers/base.ex
@@ -2,6 +2,7 @@ defmodule Procon.MessagesControllers.Base do
   defmacro __using__(_options) do
     quote do
       import Procon.MessagesControllers.Base.Helpers
+      import Procon.MessagesControllers.EventDataAccessors
 
       def create(event, options) do
         do_create(__MODULE__, event, options)
@@ -557,28 +558,5 @@ defmodule Procon.MessagesControllers.Base do
     def extract_versioned_body(event, options) do
       get_in(event, ["body", Map.get(options, :event_version) |> to_string()])
     end
-
-    def read_new_attributes(event_data), do: event_data.new_attributes
-
-    def write_new_attributes(event_data, new_attributes_map) do
-      Map.put(event_data, :new_attributes, new_attributes_map)
-    end
-
-    def read_payload(event_data), do: event_data.event.after
-
-    def read_payload_value(event_data, value_path),
-      do: get_in(event_data, [:event, :after | value_path])
-
-    def write_record(event_data, record) do
-      Map.put(event_data, :record, record)
-    end
-
-    def read_recorded_struct(event_data), do: get_in(event_data, [:recorded_struct])
-
-    def read_metadata(event_data, key),
-      do: read_recorded_struct(event_data) |> Map.get(:metadata, %{}) |> Map.get(key)
-
-    def read_recorded_struct_value(event_data, key),
-      do: read_recorded_struct(event_data) |> Map.get(key)
   end
 end
