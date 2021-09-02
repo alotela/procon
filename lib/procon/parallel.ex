56bf3aa7d693c5ba4e1e9149852ba94d7c8d0b17
charlie <commiter@papayecube.com>
2021-09-02 22:12:29 +0200

    add timeout to pmap

--- lib/procon/parallel.ex
+++ lib/procon/parallel.ex
@@ -1,7 +1,7 @@
 defmodule Procon.Parallel do
-  def pmap(collection, func) do
+  def pmap(collection, func, timeout \\ 5000) do
     collection
     |> Enum.map(&Task.async(fn -> func.(&1) end))
-    |> Enum.map(&Task.await/1)
+    |> Task.await_many(timeout)
   end
 end
