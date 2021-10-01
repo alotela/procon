9c20fbd692ddf751d69050b147d76979cdb5780c
charlie <commiter@papayecube.com>
2021-10-01 12:28:32 +0200

    add guard and warnings to  read_metadata

--- lib/procon/messages_controllers/event_data_accessors.ex
+++ lib/procon/messages_controllers/event_data_accessors.ex
@@ -23,8 +23,28 @@ defmodule Procon.MessagesControllers.EventDataAccessors do
 
   def read_recorded_struct(event_data), do: get_in(event_data, [:recorded_struct])
 
-  def read_metadata(event_data, key),
-    do: read_recorded_struct(event_data) |> Map.get(:metadata, %{}) |> Map.get(key)
+  def read_metadata(event_data, key) when is_binary(key)  do
+    Procon.Helpers.log("⚠️ To read a metadata, the key must be an atom. String received as key! (automatic convesion done, but this it to remove this warning)")
+    read_recorded_struct(event_data) |> Map.get(:metadata, %{}) |> Map.get(String.to_atom(key))
+    |> case do
+      nil ->
+        Procon.Helpers.log("⚠️ You search a metadata with key #{key} (as string), but it was not found. nil returned, no realtime message might be sent!")
+        nil
+      data ->
+        data
+      end
+  end
+
+  def read_metadata(event_data, key) when is_atom(key)  do
+    read_recorded_struct(event_data) |> Map.get(:metadata, %{}) |> Map.get(key)
+    |> case do
+      nil ->
+        Procon.Helpers.log("⚠️ You search a metadata with key #{key}, but it was not found. nil returned, no realtime message might be sent!")
+        nil
+      data ->
+        data
+      end
+    end
 
   def read_recorded_struct_value(event_data, key),
     do: read_recorded_struct(event_data) |> Map.get(key)
