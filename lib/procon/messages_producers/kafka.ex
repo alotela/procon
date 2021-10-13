54a3e3bac4a9de6885900996abefc9879d38de00
charlie <commiter@papayecube.com>
2021-10-13 11:53:44 +0200

    format

--- lib/procon/messages_producers/kafka.ex
+++ lib/procon/messages_producers/kafka.ex
@@ -1,6 +1,6 @@
 defmodule Procon.MessagesProducers.Kafka do
   def build_message(pa_key, pa_payload, pa_schema_name, pa_serialization) do
-   [serialized_key, serialized_payload] =
+    [serialized_key, serialized_payload] =
       case pa_serialization do
         :json ->
           [pa_key, Jason.encode!(pa_payload)]
